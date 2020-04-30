e7d7e4d80672a85f805173e77945efc3430490e5
DaliaPireh <Daliapireh@hotmail.com>
2020-04-30 14:24:39 +0200

    Initial commit

---
+++ src/main/java/DataKontrol.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DataKontrol {
+    public TextArea SundhedsDataOutput;
+    public TextField identifikation;
+
+    public void startDataOpsamling(ActionEvent actionEvent) {
+    }
+
+    public void startDataArkivering(ActionEvent actionEvent) {
+    }
+}
